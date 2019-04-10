0b2a2ef8ae14b76252e2caf9b2dd108a45ccee16
marzique <marzique@gmail.com>
2019-04-10 22:53:12 +0300

    reqs

--- application.py
+++ application.py
@@ -11,14 +11,11 @@ def main():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--local", action="store_true", help="localhost server")
     group.add_argument("--enterprise", action="store_true", help="working website")
-
     args = vars(parser.parse_args())
-
     if args["local"]:
         local = True
     elif args["enterprise"]:
         local = False
-
     appdata = Configer(args["URL"], local=local)
     appdata.output_configuration()
 
