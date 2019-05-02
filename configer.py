079a36a04a84408929482f7af0afdbcd9a992c1d
marzique <marzique@gmail.com>
2019-05-02 15:26:12 +0300

    XSS injector method inside class ready

--- configer.py
+++ configer.py
@@ -17,10 +17,11 @@ from helpers.helpers import strip_url
 
 class Configer:
     """Get all possible info about app configuration from it's URL"""
+
     def __init__(self, url, settings):
         self.url = url
         self.settings = settings
-        self.local = settings['local']                   # check if development mode
+        self.local = settings['local']       # check if development mode
         self.r = self.get_headers()          # http request header
         self.detected = getSimple(self.url)  # web-app confifuration
         self.cookie = self.get_cookie()
@@ -54,13 +55,11 @@ class Configer:
             sys.exit(1)
         return r
 
-
     def get_compression(self):
         try:
             return bcolors.OKGREEN + self.r.headers['Content-Encoding'] + bcolors.RESET
         except:
             return bcolors.FAIL + "hidden"
-
 
     def get_date(self):
         """Return current GMT from response header or generate it  manually"""
@@ -69,42 +68,55 @@ class Configer:
         except:
             return bcolors.OKGREEN + strftime("%a, %d %b %Y %X GMT", gmtime())
 
-
     def output_configuration(self):
         """Print human-readable results of analysis"""
         art.spin_dash(2)
-        print(bcolors.OKGREEN + "###########################################################################")
-        print(bcolors.OKGREEN + "###########################################################################")
+        print(bcolors.OKGREEN +
+              "###########################################################################")
+        print(bcolors.OKGREEN +
+              "###########################################################################")
         print()
         art.owasp_scan_header()
-        print(bcolors.OKGREEN + "###########################################################################")
-        print(bcolors.OKGREEN + "###########################################################################")
+        print(bcolors.OKGREEN +
+              "###########################################################################")
+        print(bcolors.OKGREEN +
+              "###########################################################################")
         print()
-        print(bcolors.OKGREEN + "Connecting to " + bcolors.OKBLUE + self.url  + "..." + bcolors.OKGREEN)
+        print(bcolors.OKGREEN + "Connecting to " +
+              bcolors.OKBLUE + self.url + "..." + bcolors.OKGREEN)
         art.update_progress(2)
-        print(bcolors.OKGREEN + "---------------------------------------------------------------------------")
-        print(bcolors.OKGREEN + "--------------------------CONFIGER SCAN SEARCH-----------------------------")
-        print(bcolors.OKGREEN + "---------------------------------------------------------------------------")
+        print(bcolors.OKGREEN +
+              "---------------------------------------------------------------------------")
+        print(bcolors.OKGREEN +
+              "--------------------------CONFIGER SCAN SEARCH-----------------------------")
+        print(bcolors.OKGREEN +
+              "---------------------------------------------------------------------------")
         print(bcolors.OKGREEN + f"GET REQUEST with cookie: {self.cookie}")
         print(bcolors.OKGREEN + f"Time of connection: {self.date}")
         print(bcolors.OKGREEN + f"Certificate: {self.certificate}")
         print(bcolors.OKGREEN + f"Server: {self.server}")
         print(bcolors.OKGREEN + f"Operating system: {self.os}")
         print(bcolors.OKGREEN + f"Encoding: {self.encoding}")
-        print(bcolors.OKGREEN + f"Programming language: {self.programming_lang}")
+        print(bcolors.OKGREEN +
+              f"Programming language: {self.programming_lang}")
         print(bcolors.OKGREEN + f"Compression: {self.compression}")
-        print(bcolors.OKGREEN + "------------------Search for possible admin/login pages--------------------")
+        print(bcolors.OKGREEN +
+              "------------------Search for possible admin/login pages--------------------")
         self.adminpages = search_admin_pages(self.url, progress=0, ext='php' if 'PHP' in self.programming_lang else 'a',
-        									 wordlist_file="spider/admin_login.txt")
-        print(bcolors.OKGREEN + f"admin/dashboard pages found: {len(self.adminpages)}")
-        print(bcolors.OKGREEN + "-----------------------Search for all website pages------------------------")
+                                             wordlist_file="spider/admin_login.txt")
+        print(bcolors.OKGREEN +
+              f"admin/dashboard pages found: {len(self.adminpages)}")
+        print(bcolors.OKGREEN +
+              "-----------------------Search for all website pages------------------------")
         self.pages = self.get_pages(self.url)
         print(bcolors.OKGREEN + f"Webpages found: {len(self.pages)}")
-        print(bcolors.OKGREEN + "---------------------------------------------------------------------------")
-        print(bcolors.OKGREEN + "-----------------------------CONFIGER FINISHED-----------------------------")
-        print(bcolors.OKGREEN + "---------------------------------------------------------------------------")
+        print(bcolors.OKGREEN +
+              "---------------------------------------------------------------------------")
+        print(bcolors.OKGREEN +
+              "-----------------------------CONFIGER FINISHED-----------------------------")
+        print(bcolors.OKGREEN +
+              "---------------------------------------------------------------------------")
         print(bcolors.RESET)
-
 
     def get_server(self):
         """Try to get server type from header,
@@ -118,7 +130,6 @@ class Configer:
             except:
                 return bcolors.CYAN + 'hidden' + bcolors.RESET
 
-
     def get_os(self):
         if not self.local:
             try:
@@ -128,18 +139,16 @@ class Configer:
         else:
             return bcolors.WANING + platform.platform() + bcolors.RESET + bcolors.HEADER + ' [CURRENT PC]' + bcolors.RESET
 
-
     def get_cookie(self):
         try:
             return bcolors.FAIL + self.r.headers['Set-Cookie'] + bcolors.RESET
         except KeyError:
             return bcolors.CYAN + "hidden" + bcolors.RESET
 
-
     def get_language(self):
         """Try to get programming language from header"""
         try:
-            # 1st try 
+            # 1st try
             return bcolors.FAIL + self.detected['programming-languages'] + bcolors.RESET
         except KeyError:
             try:
@@ -149,12 +158,10 @@ class Configer:
                 # TODO: https://www.owasp.org/index.php/Testing_for_HTTP_Parameter_pollution_(OTG-INPVAL-004)
                 return bcolors.CYAN + 'hidden' + bcolors.RESET
 
-
     def get_pages(self, url, no_verbose=False):
         """Return list of all webpages"""
         crawler = Crawler(url, no_verbose, limit=self.pagelimit)
         return crawler.start()
-    
 
     def check_ssl(self, url):
         """TODO"""
@@ -162,8 +169,10 @@ class Configer:
         port = 443
         try:
             cert = ssl.get_server_certificate((hostname, port))
-            x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-            date_until = str(datetime.strptime(x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'))
+            x509 = OpenSSL.crypto.load_certificate(
+                OpenSSL.crypto.FILETYPE_PEM, cert)
+            date_until = str(datetime.strptime(
+                x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'))
             return '\033[1;36mvalid until ' + date_until
         except ssl.SSLError as e:
             print(e)
--- injector.py
+++ injector.py
@@ -1,15 +1,137 @@
+from bs4 import BeautifulSoup
+import requests
+import requests.exceptions
+from urllib.parse import urlsplit
+from urllib.parse import urlparse, parse_qs
+from collections import deque
+import re
 from injections.XSS_Test import main as xss_check
+from helpers.colors import bcolors
 
 
 class Injector:
-    pass
+    """"""
 
+    def __init__(self, url):
+        self.url = url
+        # self.folder = path_to_folder
+        self.xss_links = {}  # store url: xss_snippet
+        # TODO
 
-def check_url_for_xss(url_with_get_parameters):
-    return xss_check(url_with_get_parameters)
+    ########################################################
+    ########################   XSS   #######################
+    ########################################################
+
+    def _check_url_for_xss(self, url_with_get_parameters):
+        """Check URL that has parameters for reflected XSS
+        POST&GET vector attacks.
+
+        @Return: either successful XSS(JS) snippet or None.
+        """
+        return xss_check(url_with_get_parameters)
+
+    def _get_all_links_recursive(self, url):
+        """Return all internal links from website"""
+
+        # a queue of urls to be crawled
+        new_urls = deque([url])
+
+        # a set of urls that we have already been processed
+        processed_urls = set()
+        # a set of domains inside the target website
+        local_urls = set()
+        # a set of domains outside the target website
+        foreign_urls = set()
+        # a set of broken urls
+        broken_urls = set()
+
+        # process urls one by one until we exhaust the queue
+        while len(new_urls):
+            # move next url from the queue to the set of processed urls
+            url = new_urls.popleft()
+            processed_urls.add(url)
+            # get url's content
+            # print("Processing %s" % url)
+            try:
+                response = requests.get(url)
+            except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema):
+                # add broken urls to it's own set, then continue
+                broken_urls.add(url)
+                continue
+
+            # extract base url to resolve relative links
+            parts = urlsplit(url)
+            base = "{0.netloc}".format(parts)
+            strip_base = base.replace("www.", "")
+            base_url = "{0.scheme}://{0.netloc}".format(parts)
+            path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+            # create a beutiful soup for the html document
+            soup = BeautifulSoup(response.text, "lxml")
+
+            for link in soup.find_all('a'):
+                # extract link url from the anchor
+                anchor = link.attrs["href"] if "href" in link.attrs else ''
+
+                if anchor.startswith('/'):
+                    local_link = base_url + anchor
+                    local_urls.add(local_link)
+                elif strip_base in anchor:
+                    local_urls.add(anchor)
+                elif not anchor.startswith('http'):
+                    local_link = path + anchor
+                    local_urls.add(local_link)
+                else:
+                    foreign_urls.add(anchor)
+
+                for i in local_urls:
+                    if not i in new_urls and not i in processed_urls:
+                        new_urls.append(i)
+
+        return list(processed_urls)
+
+    def _filter_parameter_pages(self, url_list):
+        """Return list of url containing at least 1 GET parameter"""
+
+        param_urls = []
+
+        for url in url_list:
+            parsed = urlparse(url)
+            if parse_qs(parsed.query):
+                param_urls.append(url)
+
+        return param_urls
+
+    def xss_attack(self):
+        """Perform xss attack on website, URL = self.url from object
+        return dict containing url: xss_snippet
+        """
+
+        all_urls = self._get_all_links_recursive(injector.url)
+        parameter_urls = self._filter_parameter_pages(all_urls)
+
+        for url in parameter_urls:
+            print(bcolors.OKGREEN + f"\nAttacking {url}")
+            result = self._check_url_for_xss(url)
+            if result:
+                self.xss_links[url] = result
+
+        return self.xss_links
+
+    ########################################################
+    ########################   SQL   #######################
+    ########################################################
+
+    ########################################################
+    ########################   XML   #######################
+    ########################################################
+
+    ########################################################
+    ####################   DESERIALIZE   ###################
+    ########################################################
 
 
 if __name__ == "__main__":
-    what_we_got = check_url_for_xss(
-        "https://xss-game.appspot.com/level1/frame?query=1")
-    print(what_we_got)
+
+    injector = Injector("http://radiophysics.herokuapp.com/")
+    print(injector.xss_attack())
