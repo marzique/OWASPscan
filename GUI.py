1de3f9005f895e78c664837d47232ba2227467c2
tarn <marzique@gmail.com>
2019-05-08 01:39:48 +0300

    requirements

--- GUI.py
+++ GUI.py
@@ -43,9 +43,9 @@ def execute():
         settings["local"] = True
         
     c = start_configer(settings, url=url)
-    # l = start_loginer(c)
-    # d = start_dependencer(folder)
-    # i = start_injector(url, folder, c.pages)
+    l = start_loginer(c)
+    d = start_dependencer(folder)
+    i = start_injector(url, folder, c.pages)
 
     time_minutes = str((time.time() - start) / 60)[:4]
     print(f"OWASPscan took {time_minutes} minutes")
